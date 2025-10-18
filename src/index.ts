17be7151cdde1941e3227f0fc4e39154db51dce3
Yuichi Sugiyama <ozwald176@gmail.com>
2025-10-18 18:19:49 +0900

    feat: add Firestore test endpoint to retrieve exhibition documents

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,20 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import health from './health.js'
 import exhibition from './exhibition.js'
+import db from './lib/firestore.js'
 
 const app = new Hono()
 
 app.route('/health', health)
 app.route('/exhibition', exhibition)
+
+app.get('/firestore-test', async (c) => {
+  const exhibitionDocumentRef = db.collection('exhibition')
+  const snapshot = await exhibitionDocumentRef.get()
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data())
+  })
+})
 
 const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 8080
 
